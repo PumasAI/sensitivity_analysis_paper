282d66f262a750557bd1e5af74ee0e9197ccd5c2
YingboMa <mayingbo5@gmail.com>
2018-09-11 19:59:07 -0400

    Add comments in `forward_sensitivity_analysis_bench.jl`

--- script/forward_sensitivity_analysis_bench.jl
+++ script/forward_sensitivity_analysis_bench.jl
@@ -1,4 +1,6 @@
-# ===============================================================
+# =============================================================== #
+# Small regime (2x3 Jacobian matrix)
+
 using DiffEqSensitivity, OrdinaryDiffEq, ForwardDiff, BenchmarkTools, StaticArrays, Profile, ProfileView
 
 function f(u, p, t)
@@ -39,6 +41,51 @@ end
 @btime auto_sen($df, $u0, $tspan, $p)
 @btime diffeq_sen($df, $u0, $tspan, $p)
 
+#======================================
+julia> @time auto_sen(f, u0s, tspan, sp)
+ 21.132961 seconds (35.99 M allocations: 1.818 GiB, 4.47% gc time)
+2×3 SArray{Tuple{2,3},Float64,2,6}:
+  2.16057   0.18857    0.563188
+ -6.25674  -0.697974  -1.70902
+
+julia> @time auto_sen(df, u0, tspan, p)
+  8.660524 seconds (18.07 M allocations: 904.867 MiB, 6.13% gc time)
+2×3 Array{Float64,2}:
+  2.16057   0.188568   0.563195
+ -6.25674  -0.697975  -1.70902
+
+julia> @time diffeq_sen(df, u0, tspan, p)
+  5.388473 seconds (12.41 M allocations: 637.880 MiB, 6.57% gc time)
+3-element Array{Array{Float64,1},1}:
+ [2.16056, -6.25677]
+ [0.188568, -0.697976]
+ [0.563185, -1.70902]
+
+julia> @btime auto_sen($f, $u0s, $tspan, $sp)
+  219.863 μs (1201 allocations: 89.85 KiB)
+2×3 SArray{Tuple{2,3},Float64,2,6}:
+  2.16057   0.18857    0.563188
+ -6.25674  -0.697974  -1.70902
+
+julia> @btime auto_sen($df, $u0, $tspan, $p)
+  239.715 μs (836 allocations: 57.65 KiB)
+2×3 Array{Float64,2}:
+  2.16057   0.188568   0.563195
+ -6.25674  -0.697975  -1.70902
+
+julia> @btime diffeq_sen($df, $u0, $tspan, $p)
+  500.736 μs (9635 allocations: 456.77 KiB)
+3-element Array{Array{Float64,1},1}:
+ [2.16056, -6.25677]
+ [0.188568, -0.697976]
+ [0.563185, -1.70902]
+
+1. autodiff is much faster than the diffeq approach.
+
+2. Using `SVector` doesn't give any significant run-time benefits, but
+increases compile time drastically
+======================================#
+
 # Profile
 #=
 auto_sen(df, u0, tspan, p)
@@ -56,7 +103,9 @@ end
 ProfileView.svgwrite("diffeq_profile.svg")
 =#
 
-# ===============================================================
+# =============================================================== #
+# Large regime (100x100 Jacobian matrix)
+
 using DiffEqSensitivity, OrdinaryDiffEq, ForwardDiff, BenchmarkTools, StaticArrays, Profile, ProfileView
 using LinearAlgebra
 
@@ -92,3 +141,26 @@ DiffEqBase.has_invW(::DiffEqSensitivity.ODELocalSensitvityFunction) = false
 @time diffeq_sen(df, u0, tspan, p)[1][1:3]
 @btime auto_sen($df, $u0, $tspan, $p);
 @btime diffeq_sen($df, $u0, $tspan, $p);
+#==========================================
+julia> @time auto_sen(df, u0, tspan, p, Rodas5(autodiff=false))[1:3]
+ 12.322556 seconds (38.20 M allocations: 2.334 GiB, 7.86% gc time)
+3-element Array{Float64,1}:
+ 0.4937800270096975
+ 0.0652016945329739
+ 0.006998118349731402
+
+julia> @time diffeq_sen(df, u0, tspan, p, Rodas5(autodiff=false))[1][1:3]
+ 98.789351 seconds (41.17 M allocations: 3.418 GiB, 0.81% gc time)
+3-element Array{Float64,1}:
+ 0.49378002795271414
+ 0.06520169515500866
+ 0.006998119064051696
+
+julia> @time auto_sen(df, u0, tspan, p, Rodas5(autodiff=false));
+  0.191787 seconds (7.57 k allocations: 20.058 MiB, 3.20% gc time)
+
+julia> @time diffeq_sen(df, u0, tspan, p, Rodas5(autodiff=false));
+106.350115 seconds (24.42 M allocations: 2.614 GiB, 0.49% gc time)
+
+# TODO: investigate allocations
+==========================================#

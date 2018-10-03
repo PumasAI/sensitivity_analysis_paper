8237c1f573549e74d126ad70b457c3469ecc7e65
Yingbo Ma <mayingbo5@gmail.com>
2018-10-02 22:01:36 -0400

    Add compile time Brusselator benchmark

--- script/forward_sensitivity_analysis_bench.jl
+++ script/forward_sensitivity_analysis_bench.jl
@@ -180,19 +180,19 @@ function makebrusselator(N=8)
     end
     Jmat = zeros(2N*N, 2N*N)
     dp = zeros(2N*N, 3)
-    @. dp[1:end÷2, 2] .= 1
     function brusselator_comp(dus, us, p, t)
         @inbounds begin
-            u, s = us[1:N*N*2], us[N*N*2+1:end]
-            du, ds = dus[1:N*N*2], dus[N*N*2+1:end]
-            @. dp[1:end÷2, 1] = -@view u[1:end÷2]
-            @. dp[end÷2+1:end, 1] = @view u[1:end÷2]
+            @views u, s = us[1:N*N*2], us[N*N*2+1:end]
+            du = @view dus[1:N*N*2]
+            fill!(dp, 0)
+            dp[1:end÷2, 1] .= -@view u[1:end÷2]
+            dp[end÷2+1:end, 1] .= @view u[1:end÷2]
             A, B, α  = p
             dfdα = @view dp[:, 3]
+            @. dp[1:end÷2, 2] .= 1
             xyd = xyd_brusselator
             dx = step(xyd)
             N = length(xyd)
-            α = α/dx^2
             II = LinearIndices((N, N, 2))
             for I in CartesianIndices((N, N))
                 x = xyd[I[1]]
@@ -201,7 +201,7 @@ function makebrusselator(N=8)
                 j = I[2]
                 ip1 = limit(i+1, N); im1 = limit(i-1, N)
                 jp1 = limit(j+1, N); jm1 = limit(j-1, N)
-                au = dfdα[II[i,j,1]] = u[II[im1,j,1]] + u[II[ip1,j,1]] + u[II[i,jp1,1]] + u[II[i,jm1,1]] - 4u[II[i,j,1]]
+                au = dfdα[II[i,j,1]] = (u[II[im1,j,1]] + u[II[ip1,j,1]] + u[II[i,jp1,1]] + u[II[i,jm1,1]] - 4u[II[i,j,1]])/dx^2
                 du[II[i,j,1]] = α*(au) + B + u[II[i,j,1]]^2*u[II[i,j,2]] - (A + 1)*u[II[i,j,1]] + brusselator_f(x, y, t)
             end
             for I in CartesianIndices((N, N))
@@ -211,21 +211,21 @@ function makebrusselator(N=8)
                 im1 = limit(i-1, N)
                 jp1 = limit(j+1, N)
                 jm1 = limit(j-1, N)
-                av = dfdα[II[i,j,2]] = u[II[im1,j,2]] + u[II[ip1,j,2]] + u[II[i,jp1,2]] + u[II[i,jm1,2]] - 4u[II[i,j,2]]
+                av = dfdα[II[i,j,2]] = (u[II[im1,j,2]] + u[II[ip1,j,2]] + u[II[i,jp1,2]] + u[II[i,jm1,2]] - 4u[II[i,j,2]])/dx^2
                 du[II[i,j,2]] = α*(av) + A*u[II[i,j,1]] - u[II[i,j,1]]^2*u[II[i,j,2]]
             end
             brusselator_jac(Jmat,u,p,t)
-            BLAS.gemm!('N', 'N', Jmat, reshape(s, N*N*2, 3), dp)
-            @. ds = dp
+            BLAS.gemm!('N', 'N', 1., Jmat, reshape(s, N*N*2, 3), 1., dp)
+            dus[N*N*2+1:end] .= vec(dp)
+            #@show t
             return nothing
         end
     end
     u0 = init_brusselator_2d(xyd_brusselator)
-    brusselator_2d_loop, u0, brusselator_jac, ODEProblem(brusselator_comp, u0, (0,10.), [3.4, 1., 10.])
+    brusselator_2d_loop, u0, brusselator_jac, ODEProblem(brusselator_comp, [u0;zeros(N*N*2*3)], (0,10.), [3.4, 1., 10.])
 end
 
 bfun, b_u0, brusselator_jac,brusselator_comp = makebrusselator(5)
-sol1 = @time solve(brusselator_comp, Tsit5(), abstol=1e-5,reltol=1e-7,save_everystep=false)
 # Run low tolerance to test correctness
 sol1 = @time auto_sen(bfun, b_u0, (0.,10.), [3.4, 1., 10.], abstol=1e-5,reltol=1e-7)
 #  8.943112 seconds (50.37 M allocations: 2.323 GiB, 9.23% gc time)
@@ -233,20 +233,25 @@ sol2 = @time diffeq_sen(bfun, b_u0, (0.,10.), [3.4, 1., 10.], abstol=1e-5,reltol
 #  13.934268 seconds (195.79 M allocations: 10.914 GiB, 16.79% gc time)
 sol3 = @time diffeq_sen(ODEFunction(bfun, jac=brusselator_jac), b_u0, (0.,10.), [3.4, 1., 10.], abstol=1e-5,reltol=1e-7)
 #  9.747963 seconds (175.60 M allocations: 10.206 GiB, 20.70% gc time)
+sol4 = @time solve(brusselator_comp, Tsit5(), abstol=1e-5,reltol=1e-7,save_everystep=false)
+#  3.850392 seconds (36.08 M allocations: 941.787 MiB, 7.35% gc time)
 
 difference1 = copy(sol1)
 difference2 = copy(sol1)
+difference3 = vec(sol1) .- vec(sol4[2][5*5*2+1:end])
 for i in eachindex(sol2)
     difference1[:, i] .-= sol2[i]
     difference2[:, i] .-= sol3[i]
 end
-@test norm(difference1) < 0.01 && norm(difference2) < 0.01
+@test norm(difference1) < 0.01 && norm(difference2) < 0.01 && norm(difference3) < 0.01
 
 # High tolerance to benchmark
-bfun_n, b_u0_n, brusselator_jacn = makebrusselator(8)
+bfun_n, b_u0_n, brusselator_jacn, b_comp = makebrusselator(8)
 @time auto_sen(bfun_n, b_u0_n, (0.,10.), [3.4, 1., 10.])
 #  13.632362 seconds (238.33 M allocations: 10.063 GiB, 15.94% gc time)
 @time diffeq_sen(bfun_n, b_u0_n, (0.,10.), [3.4, 1., 10.])
 # 302.428220 seconds (3.42 G allocations: 216.285 GiB, 12.05% gc time)
 @time diffeq_sen(ODEFunction(bfun_n, jac=brusselator_jacn), b_u0_n, (0.,10.), [3.4, 1., 10.])
 #  36.712953 seconds (442.08 M allocations: 10.215 GiB, 6.01% gc time)
+@time solve(b_comp, Tsit5(), abstol=1e-5,reltol=1e-7,save_everystep=false)
+#  12.807239 seconds (249.44 M allocations: 4.843 GiB, 10.15% gc time)

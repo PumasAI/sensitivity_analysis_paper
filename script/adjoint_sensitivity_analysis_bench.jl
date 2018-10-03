4ae260262e3b733085fd1e1e8a4b068c867453ef
Yingbo Ma <mayingbo5@gmail.com>
2018-10-03 18:45:31 -0400

    Update adjoint_sensitivity_analysis_bench

--- script/adjoint_sensitivity_analysis_bench.jl
+++ script/adjoint_sensitivity_analysis_bench.jl
@@ -10,17 +10,22 @@ u0 = [1.,1.]; tspan = (0., 10.); p = [1.5,1.0,3.0];
 
 t = 0:0.5:10
 
-# TODO: Need to re-run
 @time auto_sen_l2(df, u0, tspan, p, t, Vern9(), abstol=1e-5,reltol=1e-7)
-# 34.347232 seconds (57.73 M allocations: 3.018 GiB, 6.38% gc time)
+#  28.793466 seconds (57.92 M allocations: 3.029 GiB, 7.07% gc time)
 @time auto_sen_l2(df, u0, tspan, p, t, Vern9(); diffalg=ForwardDiff.gradient, abstol=1e-5,reltol=1e-7)
-# 11.576240 seconds (25.06 M allocations: 1.357 GiB, 9.43% gc time)
+#  9.185815 seconds (25.21 M allocations: 1.364 GiB, 8.01% gc time)
 @time diffeq_sen_l2(df, u0, tspan, p, t, Vern9(), abstol=1e-5,reltol=1e-7)
-# 20.523481 seconds (48.88 M allocations: 3.765 GiB, 8.79% gc time)
+# 17.668040 seconds (50.34 M allocations: 3.835 GiB, 9.81% gc time)
 
+# With Jacobian & paramjac
 @btime diffeq_sen_l2($df, $u0, $tspan, $p, $t, $(Vern9()), abstol=1e-5,reltol=1e-7)
-# 7.146 ms (87366 allocations: 2.12 MiB)
+#   5.477 ms (84547 allocations: 2.05 MiB)
 @btime auto_sen_l2($df, $u0, $tspan, $p, $t, $(Vern9()), abstol=1e-5,reltol=1e-7)
-# 14.232 ms (271707 allocations: 10.07 MiB)
+#   13.174 ms (271708 allocations: 10.07 MiB)
 @btime auto_sen_l2($df, $u0, $tspan, $p, $t, $(Vern9()); diffalg=$(ForwardDiff.gradient), abstol=1e-5,reltol=1e-7)
-# 116.052 μs (513 allocations: 72.96 KiB)
+#   89.042 μs (513 allocations: 72.96 KiB)
+
+df_nojac = ODEFunction(df.f)
+# Without analytical Jacobian
+@btime diffeq_sen_l2($df_nojac, $u0, $tspan, $p, $t, $(Vern9()), abstol=1e-5,reltol=1e-7)
+#   5.810 ms (84827 allocations: 2.08 MiB)

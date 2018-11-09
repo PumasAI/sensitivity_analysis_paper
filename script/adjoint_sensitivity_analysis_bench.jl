a2b49cb57d94bc89420e947de8d5e7359e3b7931
Yingbo Ma <mayingbo5@gmail.com>
2018-11-09 16:34:16 -0500

    update pollution adjoint SA

--- script/adjoint_sensitivity_analysis_bench.jl
+++ script/adjoint_sensitivity_analysis_bench.jl
@@ -74,12 +74,69 @@ DiffEqBase.has_invW(::ODELocalSensitvityFunction) = false
 DiffEqBase.has_jac(::ODELocalSensitvityFunction) = false
 
 pcomp, pu0, pp, pcompu0 = make_pollution(pollution)
-pts = 0:0.5:2
-@btime diffeq_sen_l2($(ODEFunction(pollution.f)), $pu0, $(-0.1,2.1), $pp, $pts, $(Rodas5(autodiff=false)), sensalg=$(SensitivityAlg()))
-# 2.593 s (19837898 allocations: 725.16 MiB)
-@btime diffeq_sen_l2($(ODEFunction(pollution.f)), $pu0, $(-0.1,2.1), $pp, $pts, $(Rodas5(autodiff=false)), sensalg=$(SensitivityAlg(autojacvec=false)))
-# 2.267 s (19837898 allocations: 725.16 MiB)
-@btime auto_sen_l2($(ODEFunction(pollution.f)), $pu0, $(-0.1,2.1), $pp, $pts, $(Rodas5(autodiff=false)))
-# 351.652 ms (3063903 allocations: 111.37 MiB)
-@btime auto_sen_l2($(ODEFunction(pollution.f)), $pu0, $(-0.1,2.1), $pp, $pts, $(Rodas5(autodiff=false)), diffalg=ForwardDiff.gradient)
-# 3.605 ms (1917 allocations: 453.20 KiB)
+pts = 0:0.5:60
+@time diffeq_sen_l2(ODEFunction(pollution.f), pu0, (-0.1,60.1), pp, pts, Rodas5(autodiff=false),
+                     sensalg=SensitivityAlg())
+# 75.165828 seconds (548.84 M allocations: 19.589 GiB, 10.73% gc time)
+# -1.21909  0.0163272  4.88067e-10  -167.566  77.4104  4.4199e-6  -45.7476  …  -9.66059e-7  0.000415794  -0.000841743  0.156532  2.24244e-6  -0.00126731
+@time diffeq_sen_l2(ODEFunction(pollution.f), pu0, (-0.1,60.1), pp, pts, Rodas5(autodiff=false),
+                     sensalg=SensitivityAlg(autojacvec=false)) # TODO: find out the reason which it has the exact same number of allocations, maybe I messed up?
+# 68.959907 seconds (548.84 M allocations: 19.589 GiB, 10.15% gc time)
+# 1×25 Adjoint{Float64,Array{Float64,1}}:
+#  -1.21909  0.0163272  4.88067e-10  -167.566  77.4104  4.4199e-6  -45.7476  …  -9.66059e-7  0.000415794  -0.000841743  0.156532  2.24244e-6  -0.00126731
+@btime auto_sen_l2($(ODEFunction(pollution.f)), $pu0, $(-0.1,60.1), $pp, $pts, $(Rodas5(autodiff=false)))
+# 255.377 ms (4735747 allocations: 167.20 MiB)
+#    -1.2204908070947482
+#     0.01663065506456956
+#    -1.5796224637461386e-9
+#  -168.85949034944915
+#    77.68507737751857
+#     4.443640629135168e-6
+#   -45.950810915251004
+#    -7.5723830201792884e-6
+#    -2.3389664100300814e-6
+#     4.287677550981215e-6
+#    -0.614306543296696
+#    -1.6286046860046427e-10
+#    -0.0005404691561648167
+#     7.132429401840044e-6
+#     7.385110397256353e-14
+#    -0.0007954694689778228
+#    -9.432291836475315e-8
+#    -2.7831861639726452e-15
+#     6.268437313429756e-19
+#    -9.700931271097962e-7
+#     0.00041628469961206246
+#    -0.0008421086549872315
+#     0.1564411116382127
+#     2.2430527273057995e-6
+#    -0.0012674492857014034
+
+@btime auto_sen_l2($(ODEFunction(pollution.f)), $pu0, $(-0.1,60.1), $pp, $pts, $(Rodas5(autodiff=false)),
+                   diffalg=ForwardDiff.gradient)
+# 6.524 ms (4656 allocations: 1.16 MiB)
+#   -1.2204891916144742
+#    0.01663060212953999
+#   -1.5983905312252315e-9
+# -168.8587889224989
+#   77.6850441897015
+#    4.443586172503929e-6
+#  -45.95072600694094
+#   -7.572344828012601e-6
+#   -2.338958091980496e-6
+#    4.2876607638262905e-6
+#   -0.6143198990081108
+#   -1.6284880053274313e-10
+#   -0.000540441997276013
+#    7.132414913317529e-6
+#    7.385088915972147e-14
+#   -0.0007955542566227372
+#   -9.432503569389826e-8
+#   -2.7834828464346823e-15
+#    6.269105517990765e-19
+#   -9.700906320162897e-7
+#    0.00041629349554099423
+#   -0.0008421014941126205
+#    0.15644023032995322
+#    2.2430196399649795e-6
+#   -0.0012674293032663478

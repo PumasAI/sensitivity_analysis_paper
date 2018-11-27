012d739a51ed346f8198f3c0de57d745cab06263
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-11-27 13:03:30 -0800

    nicer output

--- script/bruss_scaling.jl
+++ script/bruss_scaling.jl
@@ -25,31 +25,44 @@ forwarddiff = map(forwarddiffn) do n
   bfun, b_u0, b_p, brusselator_jac, brusselator_comp = makebrusselator(n)
   @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(ForwardDiff.gradient), save_everystep=false)
   t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(ForwardDiff.gradient), save_everystep=false)
-  @show t
+  @show n,t
 end
 reversediff = map(reversediffn) do n
   bfun, b_u0, b_p, brusselator_jac, brusselator_comp = makebrusselator(n)
   @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), diffalg=(ReverseDiff.gradient), save_everystep=false)
   t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), diffalg=(ReverseDiff.gradient), save_everystep=false)
-  @show t
+  @show n,t
 end
 numdiff = map(numdiffn) do n
   bfun, b_u0, b_p, brusselator_jac, brusselator_comp = makebrusselator(n)
   @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(DiffEqDiffTools.finite_difference_gradient), save_everystep=false)
   t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(DiffEqDiffTools.finite_difference_gradient), save_everystep=false)
-  @show t
+  @show n,t
 end
 csa = map(csan) do n
   bfun, b_u0, b_p, brusselator_jac, brusselator_comp = makebrusselator(n)
   @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), save_everystep=false, sensalg=SensitivityAlg(autojacvec=false))
   t = @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), save_everystep=false, sensalg=SensitivityAlg(autojacvec=false))
-  @show t
+  @show n,t
 end
 csaseed = map(csaseedn) do n
   bfun, b_u0, b_p, brusselator_jac, brusselator_comp = makebrusselator(n)
   @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), save_everystep=false, sensalg=SensitivityAlg(autojacvec=true))
   t = @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), save_everystep=false, sensalg=SensitivityAlg(autojacvec=true))
-  @show t
+  @show n,t
+end
+
+open("../bruss_scaling_data.txt", "w") do f
+  write(f, "forwarddiffn = $forwarddiffn")
+  write(f, "forwarddiff = $forwarddiff")
+  write(f, "reversediffn = $reversediffn")
+  write(f, "reversediff = $reversediff")
+  write(f, "numdiffn = $numdiffn")
+  write(f, "numdiff = $numdiff")
+  write(f, "csan = $csan")
+  write(f, "csa = $csa")
+  write(f, "csaseedn = $csaseedn")
+  write(f, "csaseed = $csaseed")
 end
 
 plt = plot(title="Brusselator Scaling Plot")

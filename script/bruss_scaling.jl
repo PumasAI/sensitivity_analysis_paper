922ee32bfffb70505e18bacab084992a15de557a
ChrisRackauckas <accounts@chrisrackauckas.com>
2018-11-29 05:18:13 -0500

    bruss scaling is getting close

--- script/bruss_scaling.jl
+++ script/bruss_scaling.jl
@@ -17,8 +17,8 @@ tspan = (-0.01, 1.01)
 forwarddiffn = vcat(2:10,12,15)
 reversediffn = 2:10
 numdiffn = vcat(2:10,12)
-csan = vcat(2:10,12,15:5:20)
-csaseedn = vcat(2:10,12,15:5:20)
+csan = vcat(2:10,12,15,17)
+csaseedn = vcat(2:10,12,15:5:25)
 
 println("Forward Diff")
 forwarddiff = map(forwarddiffn) do n
@@ -26,6 +26,7 @@ forwarddiff = map(forwarddiffn) do n
   @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(ForwardDiff.gradient), save_everystep=false)
   t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(ForwardDiff.gradient), save_everystep=false)
   @show n,t
+  t
 end
 println("Reverse Diff")
 reversediff = map(reversediffn) do n
@@ -33,6 +34,7 @@ reversediff = map(reversediffn) do n
   @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), diffalg=(ReverseDiff.gradient), save_everystep=false)
   t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), diffalg=(ReverseDiff.gradient), save_everystep=false)
   @show n,t
+  t
 end
 println("Num Diff")
 numdiff = map(numdiffn) do n
@@ -40,6 +42,7 @@ numdiff = map(numdiffn) do n
   @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(DiffEqDiffTools.finite_difference_gradient), save_everystep=false)
   t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(DiffEqDiffTools.finite_difference_gradient), save_everystep=false)
   @show n,t
+  t
 end
 println("CSA")
 csa = map(csan) do n
@@ -47,6 +50,7 @@ csa = map(csan) do n
   @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), save_everystep=false, sensalg=SensitivityAlg(autojacvec=false))
   t = @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), save_everystep=false, sensalg=SensitivityAlg(autojacvec=false))
   @show n,t
+  t
 end
 println("CSA Seed")
 csaseed = map(csaseedn) do n
@@ -54,22 +58,23 @@ csaseed = map(csaseedn) do n
   @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), save_everystep=false, sensalg=SensitivityAlg(autojacvec=true))
   t = @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), save_everystep=false, sensalg=SensitivityAlg(autojacvec=true))
   @show n,t
+  t
 end
 
 open("../bruss_scaling_data.txt", "w") do f
-  write(f, "forwarddiffn = $forwarddiffn")
-  write(f, "forwarddiff = $forwarddiff")
-  write(f, "reversediffn = $reversediffn")
-  write(f, "reversediff = $reversediff")
-  write(f, "numdiffn = $numdiffn")
-  write(f, "numdiff = $numdiff")
-  write(f, "csan = $csan")
-  write(f, "csa = $csa")
-  write(f, "csaseedn = $csaseedn")
-  write(f, "csaseed = $csaseed")
+  write(f, "forwarddiffn = $forwarddiffn \n")
+  write(f, "forwarddiff = $forwarddiff \n")
+  write(f, "reversediffn = $reversediffn \n")
+  write(f, "reversediff = $reversediff \n")
+  write(f, "numdiffn = $numdiffn \n")
+  write(f, "numdiff = $numdiff \n")
+  write(f, "csan = $csan \n")
+  write(f, "csa = $csa \n")
+  write(f, "csaseedn = $csaseedn \n")
+  write(f, "csaseed = $csaseed \n")
 end
 
-plt = plot(title="Brusselator Scaling Plot")
+plt = plot(title="Brusselator Scaling")
 plot!(plt, forwarddiffn, forwarddiff)
 plot!(plt, reversediffn, reversediff)
 plot!(plt, numdiffn, numdiff)

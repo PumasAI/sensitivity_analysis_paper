4516358b8d26e2d465f8bce4c3fc13f95575207f
Yingbo Ma <mayingbo5@gmail.com>
2020-12-13 13:37:10 -0500

    Update bruss scaling

--- script/bruss_scaling.jl
+++ script/bruss_scaling.jl
@@ -2,6 +2,7 @@ include("sensitivity.jl")
 include("brusselator.jl")
 
 using DiffEqSensitivity, OrdinaryDiffEq, ForwardDiff, ReverseDiff, DiffEqDiffTools
+using DiffEqSensitivity: alg_autodiff
 using LinearAlgebra
 using Test
 Base.vec(v::Adjoint{<:Real, <:AbstractVector}) = vec(v')
@@ -10,52 +11,63 @@ DiffEqBase.has_invW(::ODEForwardSensitivityFunction) = false
 DiffEqBase.has_jac(::ODEForwardSensitivityFunction) = false
 
 bt = 0:0.1:1
-tspan = (-0.01, 1.01)
+tspan = (0.0, 1.0)
 forwarddiffn = vcat(2:10,12,15)
 reversediffn = 2:10
 numdiffn = vcat(2:10,12)
 csan = vcat(2:10,12,15,17)
-csaseedn = 2:10
+#csaseedn = 2:10
+
+_adjoint_methods = ntuple(2) do ii
+  Alg = (InterpolatingAdjoint, QuadratureAdjoint)[ii]
+  (
+    user = Alg(autodiff=false,autojacvec=false), # user Jacobian
+    adjc = Alg(autodiff=true,autojacvec=false), # AD Jacobian
+    advj = Alg(autodiff=true,autojacvec=true), # AD vJ
+    advj_compiled = Alg(autodiff=true,autojacvec=ReverseDiffVJP(true)), # AD vJ compiled
+  )
+end |> NamedTuple{(:interp, :quad)}
+adjoint_methods = mapreduce(collect, vcat, _adjoint_methods)
+tols = (abstol=1e-5, reltol=1e-7)
 
 println("Forward Diff")
 forwarddiff = map(forwarddiffn) do n
   bfun, b_u0, b_p, brusselator_jac, brusselator_comp = makebrusselator(n)
-  @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(ForwardDiff.gradient))
-  t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(ForwardDiff.gradient))
+  @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()); diffalg=(ForwardDiff.gradient), tols...)
+  t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()); diffalg=(ForwardDiff.gradient), tols...)
   @show n,t
   t
 end
 println("Reverse Diff")
 reversediff = map(reversediffn) do n
   bfun, b_u0, b_p, brusselator_jac, brusselator_comp = makebrusselator(n)
-  @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), diffalg=(ReverseDiff.gradient))
-  t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), diffalg=(ReverseDiff.gradient))
+  @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)); diffalg=(ReverseDiff.gradient), tols...)
+  t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)); diffalg=(ReverseDiff.gradient), tols...)
   @show n,t
   t
 end
 println("Num Diff")
 numdiff = map(numdiffn) do n
   bfun, b_u0, b_p, brusselator_jac, brusselator_comp = makebrusselator(n)
-  @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(DiffEqDiffTools.finite_difference_gradient))
-  t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()), diffalg=(DiffEqDiffTools.finite_difference_gradient))
+  @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()); diffalg=(DiffEqDiffTools.finite_difference_gradient), tols...)
+  t = @elapsed auto_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5()); diffalg=(DiffEqDiffTools.finite_difference_gradient), tols...)
   @show n,t
   t
 end
+
 println("CSA")
 csa = map(csan) do n
   bfun, b_u0, b_p, brusselator_jac, brusselator_comp = makebrusselator(n)
-  @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), sensalg=SensitivityAlg(autojacvec=false))
-  t = @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), sensalg=SensitivityAlg(autojacvec=false))
-  @show n,t
-  t
-end
-println("CSA Seed")
-csaseed = map(csaseedn) do n
-  bfun, b_u0, b_p, brusselator_jac, brusselator_comp = makebrusselator(n)
-  @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), sensalg=SensitivityAlg(autojacvec=true))
-  t = @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, (Rodas5(autodiff=false)), sensalg=SensitivityAlg(autojacvec=true))
-  @show n,t
-  t
+  @time ts = map(adjoint_methods) do alg
+    @info "Runing $alg"
+    f = alg_autodiff(alg) ? bfun : ODEFunction(bfun, jac=brusselator_jac)
+    solver = Rodas5(autodiff=false)
+    @time diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, solver; sensalg=alg, tols...)
+    t = @elapsed diffeq_sen_l2(bfun, b_u0, tspan, b_p, bt, solver; sensalg=alg, tols...)
+    return t
+  end
+  @show n,ts
+  ts
 end
 
 open("../bruss_scaling_data.txt", "w") do f
@@ -67,6 +79,6 @@ open("../bruss_scaling_data.txt", "w") do f
   write(f, "numdiff = $numdiff \n")
   write(f, "csan = $csan \n")
   write(f, "csa = $csa \n")
-  write(f, "csaseedn = $csaseedn \n")
-  write(f, "csaseed = $csaseed \n")
+  #write(f, "csaseedn = $csaseedn \n")
+  #write(f, "csaseed = $csaseed \n")
 end

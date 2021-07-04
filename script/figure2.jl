89f30ec19fd6f2ce994be2855374682477de9ee8
Yingbo Ma <mayingbo5@gmail.com>
2021-07-04 12:05:16 -0400

    Update ploting script

--- script/figure2.jl
+++ script/figure2.jl
@@ -6,10 +6,15 @@ n_to_param(n) = 4n^2
 plt = plot(title="Sensitivity Scaling on Brusselator");
 plot!(plt, n_to_param.(forwarddiffn), forwarddiff, lab="Forward-Mode DSAAD", lw=3);
 plot!(plt, n_to_param.(reversediffn), reversediff, lab="Reverse-Mode DSAAD", lw=3);
-plot!(plt, n_to_param.(csan), csa, lab="CASA AD-Jacobian", lw=3);
-plot!(plt, n_to_param.(csaseedn), csaseed, lab=raw"CASA AD-$v^{T}J$ seeding", lw=3);
+csadata = [[csa[j][i] for j in eachindex(csa)] for i in eachindex(csa[1])]
+plot!(plt, n_to_param.(csan), csadata[1], lab="Interpolating CASA user-Jacobian", lw=3);
+plot!(plt, n_to_param.(csan), csadata[2], lab="Interpolating CASA AD-Jacobian", lw=3);
+plot!(plt, n_to_param.(csan), csadata[3], lab=raw"Interpolating CASA AD-$v^{T}J$ seeding", lw=3);
+plot!(plt, n_to_param.(csan), csadata[1+3], lab="Quadrature CASA user-Jacobian", lw=3);
+plot!(plt, n_to_param.(csan), csadata[2+3], lab="Quadrature CASA AD-Jacobian", lw=3);
+plot!(plt, n_to_param.(csan), csadata[3+3], lab=raw"Quadrature CASA AD-$v^{T}J$ seeding", lw=3);
 plot!(plt, n_to_param.(numdiffn), numdiff, lab="Numerical Differentiation", lw=3);
 plot!(plt, legend=:bottomright);
 xaxis!(plt, "Number of Parameters", :log10);
 yaxis!(plt, "Runtime (s)", :log10);
-savefig(plt, "figure2.pdf")
+savefig(plt, "../figure2.pdf")

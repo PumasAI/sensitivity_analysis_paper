4493c775eeb4e78a3e3888643c69dd8a38f8847e
Yingbo Ma <mayingbo5@gmail.com>
2021-07-05 20:58:16 -0400

    Plot update

--- script/figure2.jl
+++ script/figure2.jl
@@ -3,18 +3,31 @@ eval.(Meta.parse.(split(String(read(joinpath(@__DIR__, "..", "bruss_scaling_data
 
 n_to_param(n) = 4n^2
 
-plt = plot(title="Sensitivity Scaling on Brusselator");
-plot!(plt, n_to_param.(forwarddiffn), forwarddiff, lab="Forward-Mode DSAAD", lw=3);
-plot!(plt, n_to_param.(reversediffn), reversediff, lab="Reverse-Mode DSAAD", lw=3);
+lw = 2
+ms = 0.5
+plt1 = plot(title="Sensitivity Scaling on Brusselator");
+plot!(plt1, n_to_param.(forwarddiffn), forwarddiff, lab="Forward-Mode DSAAD", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
+plot!(plt1, n_to_param.(reversediffn), reversediff, lab="Reverse-Mode DSAAD", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
 csadata = [[csa[j][i] for j in eachindex(csa)] for i in eachindex(csa[1])]
-plot!(plt, n_to_param.(csan), csadata[1], lab="Interpolating CASA user-Jacobian", lw=3);
-plot!(plt, n_to_param.(csan), csadata[2], lab="Interpolating CASA AD-Jacobian", lw=3);
-plot!(plt, n_to_param.(csan), csadata[3], lab=raw"Interpolating CASA AD-$v^{T}J$ seeding", lw=3);
-plot!(plt, n_to_param.(csan), csadata[1+3], lab="Quadrature CASA user-Jacobian", lw=3);
-plot!(plt, n_to_param.(csan), csadata[2+3], lab="Quadrature CASA AD-Jacobian", lw=3);
-plot!(plt, n_to_param.(csan), csadata[3+3], lab=raw"Quadrature CASA AD-$v^{T}J$ seeding", lw=3);
-plot!(plt, n_to_param.(numdiffn), numdiff, lab="Numerical Differentiation", lw=3);
-plot!(plt, legend=:bottomright);
-xaxis!(plt, "Number of Parameters", :log10);
-yaxis!(plt, "Runtime (s)", :log10);
-savefig(plt, "../figure2.pdf")
+plot!(plt1, n_to_param.(csan), csadata[1], lab="Interpolating CASA user-Jacobian", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
+plot!(plt1, n_to_param.(csan), csadata[2], lab="Interpolating CASA AD-Jacobian", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
+plot!(plt1, n_to_param.(csan), csadata[3], lab=raw"Interpolating CASA AD-$v^{T}J$ seeding", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
+plot!(plt1, n_to_param.(csan), csadata[1+3], lab="Quadrature CASA user-Jacobian", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
+plot!(plt1, n_to_param.(csan), csadata[2+3], lab="Quadrature CASA AD-Jacobian", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
+plot!(plt1, n_to_param.(csan), csadata[3+3], lab=raw"Quadrature CASA AD-$v^{T}J$ seeding", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
+plot!(plt1, n_to_param.(numdiffn), numdiff, lab="Numerical Differentiation", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
+xaxis!(plt1, "Number of Parameters", :log10);
+yaxis!(plt1, "Runtime (s)", :log10);
+plot!(plt1, legend=:outertopleft, size=(600, 1000));
+savefig(plt1, "../figure2.pdf")
+
+eval.(Meta.parse.(split(String(read(joinpath(@__DIR__, "..", "bruss_vjp_data.txt"))), '\n')))
+plt2 = plot(title="Brusselator scaling with various VJPs");
+csadata = [[csavjp[j][i] for j in eachindex(csavjp)] for i in eachindex(csavjp[1])]
+plot!(plt2, n_to_param.(csan), csadata[1+3], lab=raw"EnzymeVJP", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
+plot!(plt2, n_to_param.(csan), csadata[2+3], lab=raw"ReverseDiffVJP", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
+plot!(plt2, n_to_param.(csan), csadata[3+3], lab=raw"Compiled ReverseDiffVJP", lw=lw, marksize=ms, linestyle=:auto, marker=:auto);
+xaxis!(plt2, "Number of Parameters", :log10);
+yaxis!(plt2, "Runtime (s)", :log10);
+plot!(plt2, legend=:outertopleft, size=(600, 1000));
+savefig(plt2, "../figure3.pdf")
